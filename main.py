2366847ac61807fcb70aa39a06a06567a7aa270b
Mithilesh Kapadi <mithileshkapadi08@gmail.com>
2020-11-19 17:53:06 +0530

    changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello world"
+
+@app.route('/login')
+def login():
+    return render_template('Login.html')
 
 @app.errorhandler(404)
 def not_found(e):
