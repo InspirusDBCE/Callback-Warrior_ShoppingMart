6750699ae513266256550ecf2641564f074f1a86
mithilesh1024 <60571979+mithilesh1024@users.noreply.github.com>
2020-11-19 23:43:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,85 @@
-from flask import Flask,render_template,url_for,request,session,redirect
+from flask import Flask,render_template,url_for,request,session,redirect,flash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Buyer.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = b'\xda\x96+\xccN\xadB3\xbf\x8d\x11>\xdd\x0fhn'
+db = SQLAlchemy(app)
 
+class buyer(db.Model):
+    _id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120),  nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    phoneNo = db.Column(db.Integer(), nullable=False)
+    pinCode = db.Column(db.Integer(), nullable=False)
 
+    def __init__(self,name, email, password , address, phoneNo, pinCode):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.address = address
+        self.phoneNo = phoneNo
+        self.pinCode = pinCode
+
+#ROUTES
 @app.route('/')
 def home():
     return render_template("home.html")
 
+#LOGIN
 @app.route('/login', methods=["POST", "GET"])
 def login():
-    print("Logging in")
     if request.method == "POST":
         if "new-account" in request.form:
-            print("New account")
             return redirect(url_for("create_account")) 
-        elif "remember-me" in request.form:
-            session["email"] = request.form["email"]
-            session["password"] = request.form["password"]
-            return redirect(url_for("home"))
+        email = request.form["email"]
+        password = request.form["password"]
+        foundUser = buyer.query.filter_by(email=email, password=password).first()
+        if foundUser:
+            if "remember-me" in request.form:
+                session["email"] = email
+                session["password"] = password
+                return redirect(url_for("home"))
+            else:
+                return redirect(url_for("home")) 
         else:
-            return redirect(url_for("home")) 
+            flash("Email or password is incorrect", "error")
     else:
         return render_template("Login.html")
 
+#CREATE_ACCOUNT
 @app.route("/create_account", methods=["POST", "GET"])
 def create_account():
     if request.method == "POST":
         if "sign-in" in request.form:
             return redirect(url_for("login"))
         else:
-            name = request.form["name"]
-            email = request.form["email"]
-            passowrd = request.form["password"]
-            return redirect(url_for("home"))
+            if not request.form["name"] or not request.form["email"] or not request.form["password"] or not request.form["phoneNo"] or not request.form["address"] or not request.form["pincode"]:
+                flash("Please Enter all required fields", "error")
+                return redirect(url_for("create_account"))
+            else:
+                name = request.form["name"]
+                email = request.form["email"]
+                passowrd = request.form["password"]
+                phoneNo = request.form["phoneNo"] 
+                address = request.form["address"]
+                pincode = request.form["pincode"]
+                buyers = buyer(name, email, passowrd, address, phoneNo,pincode)
+                db.session.add(buyers)
+                db.session.commit()
+                flash("Account sucessfully Created", "success")
+                return redirect(url_for("home"))
     else:
         return render_template("create_account.html")
 
-
+#ERROR HANDLING
 @app.errorhandler(404)
 def not_found(e):
     return render_template("404.html")
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
